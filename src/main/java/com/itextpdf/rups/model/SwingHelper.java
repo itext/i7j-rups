7e59933fda8ad21ca7f86c21e99c94b861691b61
Valera Goman <valera.goman@duallab.com>
2018-06-25 18:40:24 +0300

    Fix javadoc for SwingHelper
    DEVSIX-1942

--- src/main/java/com/itextpdf/rups/model/SwingHelper.java
+++ src/main/java/com/itextpdf/rups/model/SwingHelper.java
@@ -9,7 +9,7 @@ public class SwingHelper {
      * If the current thread is an AWT dispatch thread then runnable will be invoked immediately.
      * Otherwise it will be executed asynchronously.
      *
-     * @param runnable
+     * @param runnable Runnable instance whose run() method is to be invoked
      */
     public static void invoke(Runnable runnable) {
         if (SwingUtilities.isEventDispatchThread()) {
