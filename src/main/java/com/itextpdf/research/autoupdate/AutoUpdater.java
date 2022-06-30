f22ccd2afba919950790e2866da77050a7cad0b1
Matthias Valvekens <matthias.valvekens@itextpdf.com>
2022-06-30 16:08:33 +0200

    Some initial token content verification RES-635

--- src/main/java/com/itextpdf/research/autoupdate/AutoUpdater.java
+++ src/main/java/com/itextpdf/research/autoupdate/AutoUpdater.java
@@ -6,8 +6,8 @@ import com.itextpdf.kernel.pdf.PdfName;
 import com.itextpdf.kernel.pdf.PdfString;
 import com.itextpdf.rups.model.PdfFile;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -16,8 +16,8 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -51,10 +51,8 @@ public class AutoUpdater {
         return fileContent;
     }
 
-    private URL getUpdateURL() throws MalformedURLException {
-        PdfString repo = autoUpdateDict.getAsString(new PdfName("Repo"));
+    private String getResourceIdentifier() {
         PdfName addressMode = autoUpdateDict.getAsName(new PdfName("AddressMode"));
-        StringBuilder urlStr = new StringBuilder(repo.toUnicodeString());
         Encoder enc = Base64.getUrlEncoder();
         if ("ContentDigest".equals(addressMode.getValue())) {
             byte[] hash;
@@ -65,17 +63,31 @@ public class AutoUpdater {
             } catch (NoSuchAlgorithmException e) {
                 throw new IllegalStateException(e);
             }
-            urlStr.append("/hash/").append(new String(enc.encode(hash), StandardCharsets.UTF_8));
+            return new String(enc.encode(hash), StandardCharsets.UTF_8);
         } else if ("DocumentID".equals(addressMode.getValue())) {
             PdfArray arr = input.getPdfDocument().getTrailer().getAsArray(PdfName.ID);
             byte[] id1 = arr.getAsString(0).getValueBytes();
             byte[] id2 = arr.getAsString(1).getValueBytes();
 
-            urlStr.append("/docId/")
-                    .append(new String(enc.encode(id1), StandardCharsets.UTF_8))
-                    .append('/')
-                    .append(new String(enc.encode(id2), StandardCharsets.UTF_8));
+            return String.format(
+                    "%s/%s",
+                    new String(enc.encode(id1), StandardCharsets.UTF_8),
+                    new String(enc.encode(id2), StandardCharsets.UTF_8)
+            );
         }
+        throw new IllegalStateException();
+    }
+
+    private URL getUpdateURL() throws MalformedURLException {
+        PdfString repo = autoUpdateDict.getAsString(new PdfName("Repo"));
+        PdfName addressMode = autoUpdateDict.getAsName(new PdfName("AddressMode"));
+        StringBuilder urlStr = new StringBuilder(repo.toUnicodeString());
+        if ("ContentDigest".equals(addressMode.getValue())) {
+            urlStr.append("/hash/");
+        } else if ("DocumentID".equals(addressMode.getValue())) {
+            urlStr.append("/docId/");
+        }
+        urlStr.append(getResourceIdentifier());
         return new URL(urlStr.toString());
     }
 
@@ -84,11 +96,13 @@ public class AutoUpdater {
         return valueFound != null && expectedValue.equals(valueFound.getValue());
     }
 
-    private void downloadUpdate() throws IOException, UpdateVerificationException {
+    public void downloadAndApplyUpdate() throws IOException, UpdateVerificationException {
         // this method only implements a small part of the draft spec, but was written to
         // somewhat realistically represent how one would ingest an update "for real", e.g.
         // with potentially large update payloads, only exposing update content to the
         // caller after verification passes, etc.
+
+        // TODO refactor
 
         if (!nameValueIs(autoUpdateDict, "UpdateType", "Incremental")) {
             throw new IOException("Only Incremental is supported in this PoC");
@@ -96,7 +110,6 @@ public class AutoUpdater {
 
         PdfDictionary integrity = autoUpdateDict.getAsDictionary(new PdfName("Integrity"));
         PasetoV4PublicVerifier verifier;
-        MessageDigest implicitContentDigest;
         if (integrity != null) {
             boolean isPasetoV4Pub =
                     nameValueIs(integrity, "CertDataType", "PASETOV4Public");
@@ -109,25 +122,19 @@ public class AutoUpdater {
             } catch (GeneralSecurityException e) {
                 throw new UpdateVerificationException("Key deser error", e);
             }
-
-            try {
-                implicitContentDigest = MessageDigest.getInstance("SHA384");
-            } catch (NoSuchAlgorithmException e) {
-                throw new UpdateVerificationException("No SHA384", e);
-            }
         } else {
             verifier = null;
-            implicitContentDigest = null;
         }
 
         // stream the update content to disk (out of sight) while also feeding it to the verifier
         Path tempFile = Files.createTempFile("pdfupdate", ".bin");
+        long contentLength = -1;
         try (OutputStream tempOut = Files.newOutputStream(tempFile)) {
             byte[] buf = new byte[2048];
             URLConnection conn = getUpdateURL().openConnection();
             if (verifier != null) {
                 try {
-                    long contentLength = conn.getContentLengthLong();
+                    contentLength = conn.getContentLengthLong();
                     if (contentLength == -1) {
                         throw new IOException("Content-Length must be present for this PoC");
                     }
@@ -143,30 +150,39 @@ public class AutoUpdater {
                 if (verifier != null) {
                     try {
                         verifier.updateImplicit(buf, 0, bytesRead);
-                        implicitContentDigest.update(buf, 0, bytesRead);
                     } catch (GeneralSecurityException e) {
                         throw new UpdateVerificationException("Cryptographic failure", e);
                     }
                 }
             }
-
         }
 
         if (verifier != null) {
-            // byte[] contentDigest = implicitContentDigest.digest();
+            byte[] payload;
             try {
-                verifier.verifyAndGetPayload();
+                payload = verifier.verifyAndGetPayload();
             } catch (SignatureException e) {
                 throw new UpdateVerificationException("Cryptographic failure", e);
             }
+            // verify the token contents
+            // TODO do this with a schema and null-safe queries
+            JsonObject el = JsonParser
+                    .parseString(new String(payload, StandardCharsets.UTF_8))
+                    .getAsJsonObject();
+            // TODO check updateType, protocol version
+            if (!getResourceIdentifier().equals(el.getAsJsonPrimitive("resourceId").getAsString())) {
+                throw new UpdateVerificationException("Resource ID mismatch");
+            }
+            if (el.getAsJsonPrimitive("updateLength").getAsNumber().longValue()
+                    != contentLength) {
+                throw new UpdateVerificationException("Length mismatch");
+            }
+
         }
-
-
-        // TODO implement hash comparison part of integrity check
 
         // all clear -> proceed to output
         output.write(getFileContent());
-        try (InputStream tempIn = Files.newInputStream(tempFile)) {
+        try (InputStream tempIn = Files.newInputStream(tempFile, StandardOpenOption.DELETE_ON_CLOSE)) {
             byte[] buf = new byte[2048];
             int bytesRead;
             while ((bytesRead = tempIn.read(buf)) > 0) {
@@ -174,5 +190,4 @@ public class AutoUpdater {
             }
         }
     }
-
 }
