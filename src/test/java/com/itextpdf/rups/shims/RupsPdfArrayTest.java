6ab3dbd92a960fe6c8a97b5409fa87ebe06b9e08
Nick Boyd <nick.boyd@itextpdf.com>
2023-05-22 16:34:55 +0200

    Tests Started
    Signed-off-by: Nick Boyd <nick.boyd@itextpdf.com>

--- src/test/java/com/itextpdf/rups/shims/RupsPdfArrayTest.java
+++ src/test/java/com/itextpdf/rups/shims/RupsPdfArrayTest.java
@@ -1,2 +1,53 @@
-package com.itextpdf.rups.shims;public class RupsPdfArrayTest {
+package com.itextpdf.rups.shims;
+
+import com.itextpdf.kernel.pdf.PdfArray;
+import com.itextpdf.kernel.pdf.PdfName;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("UnitTest")
+@Tag("Array")
+public class RupsPdfArrayTest {
+
+    @Test
+    public void BasicNameArrayTest(){
+        PdfArray initialArray = new PdfArray();
+
+        PdfName initialValueOne = new PdfName("ValueOne");
+        PdfName initialValueTwo = new PdfName("ValueTwo");
+        initialArray.add(initialValueOne);
+        initialArray.add(initialValueTwo);
+
+        RupsPdfArray pdfArray = new RupsPdfArray(initialArray);
+
+        Assertions.assertEquals("[/ValueOne/ValueTwo]", pdfArray.toString(), "PDF Array fails to Serialize correctly.");
+
+
+    }
+    @Test
+    public void BasicNestedNameArrayTest(){
+        PdfArray initialArray = new PdfArray();
+
+        PdfArray subArrayOne = new PdfArray();
+        PdfArray subArrayTwo = new PdfArray();
+
+        PdfName initialValueOne = new PdfName("ValueOne");
+        PdfName initialValueTwo = new PdfName("ValueTwo");
+
+        subArrayOne.add(initialValueOne);
+        subArrayOne.add(initialValueTwo);
+
+        subArrayTwo.add(initialValueOne);
+        subArrayTwo.add(initialValueTwo);
+
+        initialArray.add(subArrayOne);
+        initialArray.add(subArrayTwo);
+
+        RupsPdfArray pdfArray = new RupsPdfArray(initialArray);
+
+        Assertions.assertEquals("[[/ValueOne/ValueTwo][/ValueOne/ValueTwo]]", pdfArray.toString(), "PDF Array fails to Serialize correctly.");
+
+
+    }
 }
--- src/test/java/com/itextpdf/rups/shims/RupsPdfDictionaryTest.java
+++ src/test/java/com/itextpdf/rups/shims/RupsPdfDictionaryTest.java
@@ -1,2 +1,88 @@
-package com.itextpdf.rups.shims;public class RupsPdfDictionaryTest {
+package com.itextpdf.rups.shims;
+
+import com.itextpdf.kernel.pdf.PdfArray;
+import com.itextpdf.kernel.pdf.PdfDictionary;
+import com.itextpdf.kernel.pdf.PdfName;
+import com.itextpdf.kernel.pdf.PdfString;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("UnitTest")
+@Tag("Dictionary")
+public class RupsPdfDictionaryTest {
+
+    @Test
+    public void BasicNameDictionaryTest(){
+        PdfDictionary initialDict = new PdfDictionary();
+        PdfName initialKeyOne = new PdfName("KeyOne");
+        PdfName initialValueOne = new PdfName("ValueOne");
+        PdfName initialKeyTwo = new PdfName("KeyTwo");
+        PdfName initialValueTwo = new PdfName("ValueTwo");
+
+        initialDict.put(initialKeyOne, initialValueOne);
+        initialDict.put(initialKeyTwo, initialValueTwo);
+
+        RupsPdfDictionary pdfDictionary = new RupsPdfDictionary(initialDict);
+
+        Assertions.assertEquals("<</KeyOne /ValueOne/KeyTwo /ValueTwo>>", pdfDictionary.toString(), "PDF Dictionary fails to Serialize correctly.");
+    }
+    @Test
+    public void BasicStringDictionaryTest(){
+        PdfDictionary initialDict = new PdfDictionary();
+        PdfName initialKeyOne = new PdfName("KeyOne");
+        PdfString initialValueOne = new PdfString("ValueOne");
+        PdfName initialKeyTwo = new PdfName("KeyTwo");
+        PdfString initialValueTwo = new PdfString("ValueTwo");
+
+        initialDict.put(initialKeyOne, initialValueOne);
+        initialDict.put(initialKeyTwo, initialValueTwo);
+
+        RupsPdfDictionary pdfDictionary = new RupsPdfDictionary(initialDict);
+
+        Assertions.assertEquals("<</KeyOne (ValueOne)/KeyTwo (ValueTwo)>>", pdfDictionary.toString(), "PDF Dictionary fails to Serialize correctly.");
+    }
+    @Test
+    public void NameAndNameArrayDictionaryTest(){
+        PdfDictionary initialDict = new PdfDictionary();
+        PdfArray subArray = new PdfArray();
+        PdfName initialKeyOne = new PdfName("KeyOne");
+        PdfName initialValueOne = new PdfName("ValueOne");
+        PdfName initialKeyTwo = new PdfName("KeyTwo");
+        PdfName initialValueTwo = new PdfName("ValueTwo");
+
+        subArray.add(initialValueOne);
+        subArray.add(initialValueTwo);
+
+        initialDict.put(initialKeyOne, initialValueOne);
+        initialDict.put(initialKeyTwo, subArray);
+
+        RupsPdfDictionary pdfDictionary = new RupsPdfDictionary(initialDict);
+
+        Assertions.assertEquals("<</KeyOne /ValueOne/KeyTwo [/ValueOne/ValueTwo]>>", pdfDictionary.toString(), "PDF Dictionary fails to Serialize correctly.");
+    }
+    @Test
+    public void NestedNameDictionaryTest(){
+        PdfDictionary initialDict = new PdfDictionary();
+        PdfDictionary subDictOne = new PdfDictionary();
+        PdfDictionary subDictTwo = new PdfDictionary();
+        PdfName initialKeyOne = new PdfName("KeyOne");
+        PdfName initialValueOne = new PdfName("ValueOne");
+        PdfName initialKeyTwo = new PdfName("KeyTwo");
+        PdfName initialValueTwo = new PdfName("ValueTwo");
+
+        subDictOne.put(initialKeyOne, initialValueOne);
+        subDictOne.put(initialKeyTwo, initialValueTwo);
+
+        subDictTwo.put(initialKeyOne, initialValueOne);
+        subDictTwo.put(initialKeyTwo, initialValueTwo);
+
+        initialDict.put(initialKeyOne, subDictOne);
+        initialDict.put(initialKeyTwo, subDictTwo);
+
+        RupsPdfDictionary pdfDictionary = new RupsPdfDictionary(initialDict);
+
+        Assertions.assertEquals("<</KeyOne <</KeyOne /ValueOne/KeyTwo /ValueTwo>>/KeyTwo <</KeyOne /ValueOne/KeyTwo /ValueTwo>>>>", pdfDictionary.toString(), "PDF Dictionary fails to Serialize correctly.");
+    }
+
 }
